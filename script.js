28dca8b08dfb2d53ef60363d44d37a4a3058efa9
MMOSHII <94529585+MMOSHII@users.noreply.github.com>
2025-03-08 12:09:01 +0700

    Change Date

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 1, 2025 0:00:00").getTime();
+var countDownDate = new Date("Jun 1, 2025 0:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
